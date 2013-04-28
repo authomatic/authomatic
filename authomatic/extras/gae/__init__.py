b39714fb6aaf320a781c41f44a37dded6d3db172
Peter Hudec <peterhudec@peterhudec.com>
2013-04-28 20:07:47 +0200

    Updated NDBConfig.

--- authomatic/extras/gae/__init__.py
+++ authomatic/extras/gae/__init__.py
@@ -6,11 +6,16 @@
 Utilities you can use when using this library on |gae|_.
 """
 
-from authomatic import exceptions
-from authomatic.extras import interfaces
+import os
+
 from google.appengine.ext import ndb
 from webapp2_extras import sessions
-import os
+
+from authomatic import exceptions
+from authomatic.core import resolve_provider_class
+from authomatic.extras import interfaces
+from authomatic.providers import openid
+from authomatic.extras.gae.openid import NDBOpenIDStore
 
 
 __all__ = ['ndb_config', 'Webapp2Session']
@@ -95,47 +100,50 @@ class NDBConfig(ndb.Model):
     """
     |gae| `NDB <https://developers.google.com/appengine/docs/python/ndb/>`_
     based :doc:`config`.
-    """
+    
+    .. note::
         
+        By :class:`.OpenID` provider uses :class:`.NDBOpenIDStore` as default :attr:`.OpenID.store`.
+        
+    """
+    
+    # General properties
     provider_name = ndb.StringProperty( )
     class_ = ndb.StringProperty()
     
-    # AuthorizationProvider
-    short_name = ndb.IntegerProperty()
+    # AuthorizationProvider properties
+    provider_id = ndb.IntegerProperty()
     consumer_key = ndb.StringProperty()
     consumer_secret = ndb.StringProperty()
     
-    # OAuth2
+    # OAuth2 properties
     scope = ndb.StringProperty()
+    offline = ndb.BooleanProperty()
     
-    # AuthenticationProvider
+    # AuthenticationProvider properties
     identifier_param = ndb.StringProperty()
     
-    # OpenID
-    use_realm = ndb.BooleanProperty(default=True)
-    realm_body = ndb.StringProperty()
-    realm_param = ndb.StringProperty()
-    xrds_param = ndb.StringProperty()
-    sreg = ndb.StringProperty()
-    sreg_required = ndb.StringProperty()
-    ax = ndb.StringProperty()
-    ax_required = ndb.StringProperty()
-    pape = ndb.StringProperty()
     
     @classmethod
     def get(cls, key, default=None):
         """
-        Resembles the dict.get(key[, default=None]) method
+        Resembles the :meth:`dict.get` method.
         
-        Returns a provider config dictionary
+        :returns:
+            A configuration dictionary for specified provider.
         """
         
+        # Query datastore.
         result = cls.query(cls.provider_name == key).get()
         
         if result:
             result_dict = result.to_dict()
             
-            for i in ('scope', 'sreg', 'sreg_required', 'ax', 'ax_required', 'pape', ):
+            # Use NDBOpenIDStore by default
+            result_dict['store'] = NDBOpenIDStore
+            
+            # Convert coma-separated values to list. Currently only scope is csv.
+            for i in ('scope', ):
                 prop = result_dict.get(i)
                 if prop:
                     result_dict[i] = [s.strip() for s in prop.split(',')]
@@ -149,6 +157,10 @@ class NDBConfig(ndb.Model):
     
     @classmethod
     def values(cls):
+        """
+        Resembles the :meth:`dict.values` method.
+        """
+        
         # get all items
         results = cls.query().fetch()
         # return list of dictionaries
@@ -158,7 +170,15 @@ class NDBConfig(ndb.Model):
     @classmethod
     def initialize(cls):
         """
-        Creates an Example entity if the model is empty
+        Creates an **"Example"** entity of kind **"NDBConfig"** in the datastore
+        if the model is empty and raises and error to inform you that you should populate the model with data.
+        
+        .. note::
+        
+            The *Datastore Viewer* in the ``_ah/admin/`` won't let you add properties to a model
+            if there is not an entity with that property allready.
+            Therefore it is a good idea to keep the **"Example"** entity (which has all
+            possible properties set) in the datastore.
         """
         
         if not len(cls.query().fetch()):
@@ -168,38 +188,23 @@ class NDBConfig(ndb.Model):
             example.class_ = 'Provider class e.g. "authomatic.providers.oauth2.Facebook".'
             example.provider_name = 'Your custom provider name e.g. "fb".'
             
-            # TODO: Implement all new arguments including openid store.
-            
             # AuthorizationProvider
             example.consumer_key = 'Consumer key.'
             example.consumer_secret = 'Consumer secret'
-            example.short_name = 1
+            example.provider_id = 1
             
             # OAuth2
             example.scope = 'coma, separated, list, of, scopes'
             
             # AuthenticationProvider
-            example.identifier_param = 'User identifier to authenticate e.g. "me.yahoo.com".'
+            example.identifier_param = 'Querystring parameter for claimed id. default is "id"'
             
-            # OpenID
-            example.use_realm = True
-            example.realm_body = 'Contents of the HTML body tag of the realm.'
-            example.realm_param = 'Name of the query parameter to be used to serve the realm (leave empty for default).'
-            example.xrds_param = ' The name of the query parameter to be used to serve the XRDS document (leave empty for default).'
-            example.sreg = 'list, of, strings, of, optional, SREG, fields, (leave empty for defaults)'
-            example.sreg_required = 'list, of, strings, of, required, SREG, fields, (leave empty for defaults)'
-            example.ax = 'list, of, strings, of, optional, AX, schemas, (leave empty for defaults)'
-            example.ax_required = 'list, of, strings, of, required, AX, schemas, (leave empty for defaults)'
-            example.pape = 'list, of, strings, of, optional, PAPE, policies, (leave empty for defaults)'
-            
+            # Save the example
             example.put()
             
-            url = '{}://{}/_ah/admin/datastore?kind={}'.format(os.environ.get('wsgi.url_scheme'),
-                                                               os.environ.get('HTTP_HOST'),
-                                                               cls.__name__)
-            
-            raise GAEError('A NDBConfig data model was created! ' + \
-                           'Go to {} and populate it with data!'.format(url))
+            # Raise an information error.
+            raise GAEError('A NDBConfig data model was created! Go to Datastore Viewer ' + \
+                           'in your dashboard and populate it with data!')
 
 
 def ndb_config():
