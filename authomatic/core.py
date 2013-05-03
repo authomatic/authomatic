fb3f6f97ce5ba44acb3ce7908a1ffc12fe086fb4
Peter Hudec <peterhudec@peterhudec.com>
2013-05-03 19:05:03 +0200

    Improved comments.

--- authomatic/core.py
+++ authomatic/core.py
@@ -887,7 +887,35 @@ class LoginResult(ReprMixin):
         #: An instance of the :exc:`authomatic.exceptions.BaseError` subclass.
         self.error = None
     
+    
     def popup_js(self, callback_name=None, indent=None, custom=None, stay_open=False):
+        """
+        Returns JavaScript that:
+        
+        #.  Triggers the ``options.onLoginComplete`` handler set with the
+            :ref:`authomatic.setup() <js_setup>` function of :ref:`javascript.js <js>`.
+        #.  Calls the JavasSript callback specified by :data:`callback_name`
+            on the opener of the *login handler popup* with a ``result`` JSON object passed to it.
+        #.  Closes itself.
+        
+        :param str callback_name:
+            The name of the javascript callback e.g ``foo.bar.loginCallback``
+            will result in ``window.opener.foo.bar.loginCallback(result);`` in the HTML.
+        
+        :param int indent:
+            The number of spaces to indent the JSON result object.
+            If ``0`` or negative, only newlines are added.
+            If ``None``, no newlines are added.
+            
+        :param custom:
+            Any JSON serializable object that will be passed to the ``result.custom`` attribute.
+        
+        :param str stay_open:
+            If ``True``, the popup will stay open.
+        
+        :returns:
+            :class:`str` with JavaScript.
+        """
         
         custom_callback = """
         if (typeof window.opener.{cb} === "function") {{
@@ -914,7 +942,7 @@ class LoginResult(ReprMixin):
                    stay_open='// ' if stay_open else '')
     
     
-    def popup_html(self, callback_name=None, indent=None, title='Login | {}', custom=None):
+    def popup_html(self, callback_name=None, indent=None, title='Login | {}', custom=None, stay_open=False):
         """
         Returns a HTML with JavaScript that:
         
@@ -940,6 +968,9 @@ class LoginResult(ReprMixin):
         :param custom:
             Any JSON serializable object that will be passed to the ``result.custom`` attribute.
         
+        :param str stay_open:
+            If ``True``, the popup will stay open.
+        
         :returns:
             :class:`str` with HTML.
         """
@@ -955,7 +986,7 @@ class LoginResult(ReprMixin):
             </body>
         </html>
         """.format(title=title.format(self.provider.name if self.provider else ''),
-                   js=self.popup_js(callback_name, indent, custom))
+                   js=self.popup_js(callback_name, indent, custom, stay_open))
     
     @property
     def user(self):
