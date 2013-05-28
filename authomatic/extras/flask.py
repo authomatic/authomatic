97ffc1c3d65a927245b1baa1d4f6a0a3b270c83d
Mark Steve Samson <blazinggale@gmail.com>
2013-05-28 10:12:31 +0800

    Used `absolute_import` to resolve module name conflict issue instead of renaming the module.

--- authomatic/extras/flask.py
+++ authomatic/extras/flask.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 from functools import wraps
 
 from authomatic.adapters import WerkzeugAdapter
--- examples/flask/main.py
+++ examples/flask/main.py
@@ -1,6 +1,6 @@
 import logging
 
-from authomatic.extras.flask_authomatic import FlaskAuthomatic
+from authomatic.extras.flask import FlaskAuthomatic
 from authomatic.providers import oauth2
 from flask import Flask, jsonify
 
