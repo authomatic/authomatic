0b007ca5f0ed7492758a5c7597db55968501d2c3
Peter Hudec <peterhudec@peterhudec.com>
2013-06-20 15:28:38 +0200

    Increased the release version to 0.0.6.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name='Authomatic',
-    version='0.0.5',
+    version='0.0.6',
     packages=find_packages(),
     package_data={'': ['*.txt', '*.rst']},
     author='Peter Hudec',
