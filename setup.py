e741f4e1702745948c6590f7ac9eb9a3e24f1070
Peter Hudec <peterhudec@peterhudec.com>
2013-07-03 20:12:28 +0200

    Updated release number to 0.0.7.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name='Authomatic',
-    version='0.0.6',
+    version='0.0.7',
     packages=find_packages(),
     package_data={'': ['*.txt', '*.rst']},
     author='Peter Hudec',
