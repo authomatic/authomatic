99056accff33065ea3ee2a2c2825dd7b0826a52c
Peter Hudec <peterhudec@peterhudec.com>
2014-04-15 21:57:07 +0200

    Migrated to Foundation 5.
    Added style for Xing button.

--- examples/gae/showcase/static/config.rb
+++ examples/gae/showcase/static/config.rb
@@ -1,5 +1,5 @@
 # Require any additional compass plugins here.
-require 'zurb-foundation'
+add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
