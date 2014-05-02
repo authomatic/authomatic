61d8feeecd5e583ac83695e3422e34201eb9962d
Peter Hudec <peterhudec@peterhudec.com>
2014-05-02 18:35:28 +0200

    Increased the release version to 0.0.9.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -124,7 +124,7 @@ copyright = u'2013, Peter Hudec'
 # built documents.
 #
 # The short X.Y version.
-version = '0.0.8'
+version = '0.0.9'
 # The full version, including alpha/beta/rc tags.
 release = version
 
