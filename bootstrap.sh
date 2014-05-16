1bad68958996250d381bee047af5f8d88627e04c
Peter Hudec <peterhudec@peterhudec.com>
2014-05-16 20:29:13 +0200

    Updated bootstrap.sh to init and build git submodules and compile docs.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,17 @@
+# Update git submodules
+git submodule init
+git submodule update
+
+# Create and activate virtual environment "./venv"
 python bootstrap/makebootstrap.py
 python bootstrap/bootstrap.py venv
 . venv/bin/activate
+
+# Install requirements
 pip install -r requirements.txt
+
+# Compile documentation
+make html -C doc/
+
+# Deactivate virtual environment
 deactivate
