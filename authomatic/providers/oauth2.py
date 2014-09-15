220ce36983dff338c52f5afbc8e85e04cbc17160
Paul Brown <paul90brown@gmail.com>
2014-09-14 21:55:18 -0500

    Add eventbrite support to oauth2 providers

--- authomatic/providers/oauth2.py
+++ authomatic/providers/oauth2.py
@@ -594,7 +594,54 @@ class DeviantART(OAuth2):
         user.picture = data.get('usericonurl')
         return user
 
+class Eventbrite(OAuth2):
+    """
+    Eventbrite |oauth2| provider.
+    
+    * Dashboard: http://www.eventbrite.com/myaccount/apps/
+    * Docs: http://developer.eventbrite.com/docs/
+    """
 
+    user_authorization_url = 'https://www.eventbrite.com/oauth/authorize'
+    access_token_url = 'https://www.eventbrite.com/oauth/token'
+    user_info_url = 'https://www.eventbriteapi.com/v3/users/me'
+    
+    supported_user_attributes = core.SupportedUserAttributes(
+        id=True,
+        email=True,
+        name=True,
+        first_name=True,
+        last_name=True,
+    )
+    
+    type_id = 100017 # prevents AttributeError: 'module' object has no attribute 'PROVIDER_ID_MAP'
+    
+    @classmethod
+    def _x_credentials_parser(cls, credentials, data):
+        credentials.token = data.get('access_token')
+        if data.get('token_type') == 'bearer':
+            credentials.token_type = cls.BEARER # prevents ValueError: u'bearer' is not in list
+        return credentials
+    
+    @staticmethod
+    def _x_user_parser(user, data):
+        """
+        Use this to populate the User object with data from JSON
+        returned by provider on User.update().
+        """      
+        
+        user.id = data.get('id')
+        _emails = data.get('emails', [])
+        for email in _emails:
+            if email.get('type', '') == 'primary':
+                user.email = email.get('address')
+                break
+        user.first_name = data.get('first_name')
+        user.last_name = data.get('last_name')
+        user.name = data.get('name')
+        
+        return user
+        
 class Facebook(OAuth2):
     """
     Facebook |oauth2| provider.
