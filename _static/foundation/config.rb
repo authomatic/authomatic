b486b0b23289f1fed072373fee23e6b01249acea
Peter Hudec <peterhudec@peterhudec.com>
2014-10-08 20:45:40 +0200

    Updated GitHub Page.

---
+++ _static/foundation/config.rb
@@ -0,0 +1,35 @@
+# Require any additional compass plugins here.
+require 'zurb-foundation'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+
+# local
+css_dir = "css"
+
+# sample project
+# css_dir = "../../../sample-project/build/html/_static/foundation/css"
+
+# gh-page basic
+# css_dir = "../../../gh-page/basic/html/_static/foundation/css"
+
+# gh-page cards
+# css_dir = "../../../gh-page/cards/html/_static/foundation/css"
+
+# Authomatic
+# css_dir = "../../../../authomatic/doc/build/html/_static/foundation/css"
+
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+preferred_syntax = :sass
--- examples/django/example/run_example.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Activate virtual environment.
-. ../../../venv/bin/activate
-
-# Start the django development server.
-python manage.py runserver 8080
