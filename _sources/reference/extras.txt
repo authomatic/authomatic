76c9fbb1a2cd73c461bd106df26a4c51c4fe4806
Peter Hudec <peterhudec@peterhudec.com>
2014-10-08 20:50:04 +0200

    Updated gh-page.

---
+++ _sources/reference/extras.txt
@@ -0,0 +1,19 @@
+Extras
+======
+
+.. seo-description::
+
+	There is extra stuff available for some frameworks and platforms in addition to adapters.
+
+.. contents::
+   :backlinks: none
+
+.. automodule:: authomatic.extras.flask
+   :members:
+
+.. automodule:: authomatic.extras.gae
+   :members:
+
+.. automodule:: authomatic.extras.interfaces
+   :members:
+
---
+++ _static/config.rb
@@ -0,0 +1,26 @@
+# Require any additional compass plugins here.
+require 'zurb-foundation'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+
+# local
+css_dir = "css"
+
+# build
+# css_dir = "../../build/html/_static/css"
+
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+preferred_syntax = :sass
