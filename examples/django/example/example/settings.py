7e6b02dabfc4969d5ecfc080b29e3439d127ccc6
Peter Hudec <peterhudec@peterhudec.com>
2014-11-18 19:18:51 +0100

    Added a template with link to simple example to the django example project homepage.

--- examples/django/example/example/settings.py
+++ examples/django/example/example/settings.py
@@ -1,8 +1,11 @@
 # example/example/settings.py
 # Django settings for example project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -107,6 +110,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'templates'),
 )
 
 INSTALLED_APPS = (
