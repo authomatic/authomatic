42ed9335ac937b2eecb46f98f133fe54303be503
Peter Hudec <peterhudec@peterhudec.com>
2015-03-29 00:15:07 +0100

    Increased the version number to 0.0.11.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from authomatic import six
 
 setup(
     name='Authomatic',
-    version='0.0.10', # TODO: Put version in one place.
+    version='0.0.11', # TODO: Put version in one place.
     packages=find_packages(),
     package_data={'': ['*.txt', '*.rst']},
     author='Peter Hudec',
