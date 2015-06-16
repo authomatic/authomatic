c9efe2a471b71ff14eca026da193943dd20f6fad
Peter Hudec <peterhudec@peterhudec.com>
2015-06-16 15:00:59 +0200

    Switched to requests dependency.

--- tests/functional_tests/travis2.py
+++ tests/functional_tests/travis2.py
@@ -1,14 +1,21 @@
-import httplib
+# import httplib
+#
+#
+#
+# # print urlopen('https://authomatic.com:8080', context=ctx).read()
+#
+# connection = httplib.HTTPSConnection('authomatic.com', 8080)
+#
+# connection.request('GET', '/')
+#
+# response = connection.getresponse()
+#
+# print response.status
+# print response.read()
 
 
+import requests
 
-# print urlopen('https://authomatic.com:8080', context=ctx).read()
-
-connection = httplib.HTTPSConnection('authomatic.com', 8080)
-
-connection.request('GET', '/')
-
-response = connection.getresponse()
-
-print response.status
-print response.read()
+r = requests.get('https://authomatic.com:8080', verify=False)
+print(r.status_code)
+print(r.text)
