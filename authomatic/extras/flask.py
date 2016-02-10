927e54666522cf3167e5ed9bcb24c837cb108aea
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2016-02-10 18:51:30 +0100

    Add adapter_cls argument to FlaskAuthomatic

--- authomatic/extras/flask.py
+++ authomatic/extras/flask.py
@@ -20,20 +20,26 @@ from authomatic import Authomatic
 class FlaskAuthomatic(Authomatic):
     """
     Flask Plugin for authomatic support.
+
+    :param adapter_cls: Adapter class to use (default: `WerkzeugAdapter`)
     """
 
     result = None
+
+    def __init__(self, *args, **kwargs):
+        self._adapter_cls = kwargs.pop('adapter_cls', WerkzeugAdapter)
+        super(FlaskAuthomatic, self).__init__(*args, **kwargs)
 
     def login(self, *login_args, **login_kwargs):
         """
         Decorator for Flask view functions.
         """
-        
+
         def decorator(f):
             @wraps(f)
             def decorated(*args, **kwargs):
                 self.response = make_response()
-                adapter = WerkzeugAdapter(request, self.response)
+                adapter = self._adapter_cls(request, self.response)
                 login_kwargs.setdefault('session', session)
                 login_kwargs.setdefault('session_saver', self.session_saver)
                 self.result = super(FlaskAuthomatic, self).login(adapter, *login_args, **login_kwargs)
