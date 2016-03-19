5cb04a4f95550f0e62cd03e9355189a73bf04cdc
Peter Hudec <peterhudec@peterhudec.com>
2016-03-19 21:52:36 +0000

    Increased release version to 0.1.1.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from authomatic import six
 
 setup(
     name='Authomatic',
-    version='0.1.0.post1', # TODO: Put version in one place.
+    version='0.1.1', # TODO: Put version in one place.
     packages=find_packages(),
     package_data={'': ['*.txt', '*.rst']},
     author='Peter Hudec',
