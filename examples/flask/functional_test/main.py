e9565eb1e2c6bdea72989e8ed62dc5e5094ba518
Peter Hudec <peterhudec@peterhudec.com>
2016-06-26 19:55:40 +0100

    The functional_tests Flask app now logs to the ./flask.log file.

--- examples/flask/functional_test/main.py
+++ examples/flask/functional_test/main.py
@@ -40,6 +40,14 @@ def login(provider_name):
 
 
 if __name__ == '__main__':
+
+    # if not app.debug:
+    import logging
+
+    file_handler = logging.FileHandler('flask.log')
+    # file_handler.setLevel(logging.WARNING)
+    app.logger.addHandler(file_handler)
+
     # This does nothing unles you run this module with --testliveserver flag.
     import liveandletdie
     liveandletdie.Flask.wrap(app)
