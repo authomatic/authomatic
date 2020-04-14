eb54dd5fdbb0a36964cc439ef522375b77f7e3a8
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2020-04-14 10:59:12 -0400

    update setuptools method to specify python version + remove tests requirements (let tox define them

--- setup.py
+++ setup.py
@@ -2,20 +2,16 @@
 from setuptools import find_packages
 from setuptools import setup
 
+oid_reqs = [
+    "python-openid ; python_version<'3'",
+    "python3-openid ; python_version>='3'",
+]
+
 setup(
     packages=find_packages(),
     package_data={'': ['*.txt', '*.rst']},
     extras_require={
-        'OpenID': [
-            "python-openid ; python_version<'3'",
-            "python3-openid ; python_version>='3'",
-        ],
-        'test': [
-            "jinja2",
-            "liveandletdie",
-            "pytest",
-            "pyvirtualdisplay",
-            "selenium",
-        ]
+        'OpenID: python_version < "3"': ['python-openid'],
+        'OpenID: python_version >= "3"': ['python3-openid'],
     },
 )
